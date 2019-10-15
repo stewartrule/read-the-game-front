0f26ad00e5774fde235f124f04e5df094315e8e2
Walter Steur <stewartrule@gmail.com>
2019-10-15 20:53:36 +0200

    Add SimplePercentageBar component

---
+++ src/components/SimplePercentageBar/SimplePercentageBar.tsx
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+import * as React from "react";
+
+const Segment = ({
+  value,
+  type
+}: {
+  value: number;
+  type: "left" | "right";
+}) => (
+  <div
+    style={{ width: `${value * 100}%` }}
+    className={`simple-percentage-bar__segment simple-percentage-bar__segment--${type}`}
+  ></div>
+);
+
+const PercentageBar = ({ left, right }: { left: number; right: number }) => (
+  <div className="simple-percentage-bar">
+    <Segment value={left} type="left" />
+    <Segment value={right} type="right" />
+  </div>
+);
+
+export default PercentageBar;
