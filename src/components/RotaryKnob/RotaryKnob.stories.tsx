ba958cbd5349d6f5e7d3fa9f8c7145c6bc0e7bea
Walter Steur <stewartrule@gmail.com>
2019-10-15 20:50:38 +0200

    Add RotaryKnob stories

---
+++ src/components/RotaryKnob/RotaryKnob.stories.tsx
@@ -0,0 +1,62 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import { BrandColor, FontFamily } from "../../util/skin";
+import Section from "../Section";
+import RotaryKnob, { RenderOptions } from "./RotaryKnob";
+
+const { useState } = React;
+
+storiesOf("RotaryKnob", module)
+  .addDecorator(getStory => <Section>{getStory()}</Section>)
+  .add("Render value", () => {
+    const [value, setValue] = useState(0);
+
+    return (
+      <RotaryKnob
+        value={value}
+        onDrag={newValue => {
+          setValue(newValue);
+        }}
+        render={props => <InsideText {...props} />}
+      />
+    );
+  })
+  .add("Render radius", () => {
+    const [value, setValue] = useState(0);
+
+    return (
+      <RotaryKnob
+        value={value}
+        onDrag={newValue => {
+          setValue(newValue);
+        }}
+        render={props => <InsideRadius {...props} />}
+      />
+    );
+  });
+
+const InsideText = ({ center, value, dragged }: RenderOptions) => (
+  <text
+    x={center.x}
+    y={center.y}
+    textAnchor="middle"
+    alignmentBaseline="middle"
+    fontFamily={FontFamily.Default}
+    fontWeight="bold"
+    style={{ textAlign: "center" }}
+    fontSize="96px"
+    fill={dragged ? BrandColor.secondary : BrandColor.primary}
+  >
+    {(value * 100).toFixed(1)}
+  </text>
+);
+
+const InsideRadius = ({ center, value, radius, dragged }: RenderOptions) => (
+  <circle
+    cx={center.x}
+    cy={center.y}
+    r={value * radius}
+    fill={dragged ? BrandColor.secondary : BrandColor.primary}
+  />
+);
