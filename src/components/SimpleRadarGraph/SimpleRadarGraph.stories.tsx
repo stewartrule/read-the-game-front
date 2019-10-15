b655e334cb6dcd63175e9b8d58bdf686f5846707
Walter Steur <stewartrule@gmail.com>
2019-10-15 20:54:38 +0200

    Add SimpleRadarGraph stories

---
+++ src/components/SimpleRadarGraph/SimpleRadarGraph.stories.tsx
@@ -0,0 +1,30 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import Section from "../Section";
+import SimpleRadarGraph, { Stat } from "./SimpleRadarGraph";
+
+const stats: Stat[] = [
+  { label: "A", value: 1 },
+  { label: "B", value: 0.6 },
+  { label: "C", value: 1 },
+  { label: "D", value: 1 },
+  { label: "E", value: 0.85 },
+  { label: "F", value: 1 }
+];
+
+storiesOf("SimpleRadarGraph", module)
+  .addDecorator(getStory => <Section>{getStory()}</Section>)
+  .add("Background", () => (
+    <>
+      <Section primary>
+        <SimpleRadarGraph stats={stats} />
+      </Section>
+      <Section dark>
+        <SimpleRadarGraph stats={stats} />
+      </Section>
+      <Section secondary>
+        <SimpleRadarGraph stats={stats} />
+      </Section>
+    </>
+  ));
