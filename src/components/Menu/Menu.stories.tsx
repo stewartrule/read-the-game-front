f48e7825dafd2e7d0fe6a487c712c56e26ef9f66
Walter Steur <stewartrule@gmail.com>
2019-10-15 20:44:13 +0200

    Add Menu stories

---
+++ src/components/Menu/Menu.stories.tsx
@@ -0,0 +1,26 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import Section from "../Section";
+import Menu from "./";
+import MenuItem from "./MenuItem";
+
+storiesOf("Menu", module)
+  .addDecorator(getStory => (
+    <Section secondary compact>
+      {getStory()}
+    </Section>
+  ))
+  .add("Menu", () => (
+    <Menu head="Kein Vergleich">
+      <MenuItem>Borussia Dortmund</MenuItem>
+      <MenuItem>Borussia Mönchengladbach</MenuItem>
+      <MenuItem>Eintracht Frankfurt</MenuItem>
+      <MenuItem>FC Augsburg</MenuItem>
+      <MenuItem>FC Bayern München</MenuItem>
+      <MenuItem>FC Ingolstadt 04</MenuItem>
+      <MenuItem>FC Schalke 04</MenuItem>
+      <MenuItem active>RB Leipzig</MenuItem>
+      <MenuItem>Hamburger SV</MenuItem>
+    </Menu>
+  ));
