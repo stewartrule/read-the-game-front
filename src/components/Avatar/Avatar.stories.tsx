13611d1febfd74f3c5e82947a7019b8333369aa0
Walter Steur <stewartrule@gmail.com>
2019-10-15 20:34:35 +0200

    Add Avatar stories

---
+++ src/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,58 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import { BrandColor } from "../../util/skin";
+import Section from "../Section";
+import Avatar from "./";
+
+const players = [
+  {
+    id: 4,
+    shots: [{ id: 1, hit: true }, { id: 2, hit: false }, { id: 3, hit: false }]
+  },
+  {
+    id: 5,
+    shots: [{ id: 1, hit: true }, { id: 2, hit: true }, { id: 3, hit: false }]
+  },
+  {
+    id: 6,
+    shots: [{ id: 1, hit: true }, { id: 2, hit: true }, { id: 3, hit: true }]
+  }
+];
+
+const mapped = players.map(({ id, shots }, i) => {
+  return {
+    id,
+    value: 0.2 + i * 0.3,
+    image: "http://thetopforward.com/uploads/0/57134.jpg"
+  };
+});
+
+storiesOf("Avatar", module)
+  .addDecorator(getStory => <Section>{getStory()}</Section>)
+  .add("Primary", () => (
+    <>
+      {mapped.map((player, i) => (
+        <Avatar
+          key={player.id}
+          image={player.image}
+          value={player.value}
+          color={BrandColor.primary}
+          radius={30}
+        />
+      ))}
+    </>
+  ))
+  .add("Secondary", () => (
+    <>
+      {mapped.map(player => (
+        <Avatar
+          key={player.id}
+          image={player.image}
+          value={player.value}
+          color={BrandColor.secondary}
+          radius={30}
+        />
+      ))}
+    </>
+  ));
