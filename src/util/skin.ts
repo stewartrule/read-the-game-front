439c0541eeaf563581eed7e6629a3cfeab21b460
Walter Steur <stewartrule@gmail.com>
2019-10-15 20:59:29 +0200

    Add skin utility types

---
+++ src/util/skin.ts
@@ -0,0 +1,12 @@
+export enum FontFamily {
+  Montserrat = "Montserrat",
+  Default = "Montserrat"
+}
+
+export enum BrandColor {
+  primary = "#0076ff",
+  secondary = "#42dd84",
+  light = "#f3f3f3",
+  black = "#000",
+  white = "#fff"
+}
