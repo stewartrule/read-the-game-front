9f34e181f781d4b49ca4888c666f22c383400a52
Walter Steur <stewartrule@gmail.com>
2019-10-17 20:30:40 +0200

    Add knobs to PeriodGraph story

--- src/components/PeriodGraph/PeriodGraph.stories.tsx
+++ src/components/PeriodGraph/PeriodGraph.stories.tsx
@@ -1,3 +1,4 @@
+import { number, withKnobs } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
@@ -6,69 +7,46 @@ import { Tuple } from "../../util/types";
 import Section from "../Section";
 import Graph, { ValueType } from "./PeriodGraph";
 
-const values: Tuple<ValueType, 6> = [
-  {
-    inner: {
-      fill: BrandColor.secondary,
-      value: 1
-    },
-    outer: {
-      fill: BrandColor.primary,
-      value: 0.1
-    }
-  },
-  {
-    inner: {
-      fill: BrandColor.primary,
-      value: 0.9
-    },
-    outer: {
-      fill: BrandColor.secondary,
-      value: 0.4
-    }
-  },
-  {
-    inner: {
-      fill: BrandColor.secondary,
-      value: 0.8
-    },
-    outer: {
-      fill: BrandColor.primary,
-      value: 0.6
-    }
-  },
-  {
-    inner: {
-      fill: BrandColor.primary,
-      value: 0.7
-    },
-    outer: {
-      fill: BrandColor.secondary,
-      value: 0.8
-    }
-  },
-  {
-    inner: {
-      fill: BrandColor.primary,
-      value: 0.5
-    },
-    outer: {
-      fill: BrandColor.secondary,
-      value: 0.8
-    }
-  },
-  {
-    inner: {
-      fill: BrandColor.primary,
-      value: 0.2
-    },
-    outer: {
-      fill: BrandColor.secondary,
-      value: 0.8
-    }
-  }
-];
-
 storiesOf("PeriodGraph", module)
   .addDecorator(getStory => <Section spacious>{getStory()}</Section>)
-  .add("todo", () => <Graph values={values} />);
+  .addDecorator(withKnobs)
+  .add("PeriodGraph", () => {
+    const values: ValueType[] = Array.from({ length: 6 }, (_, i) => {
+      const inner = number(
+        `Inner`,
+        Math.round(Math.random() * 10) / 10,
+        {
+          range: true,
+          min: 0,
+          max: 1,
+          step: 0.1
+        },
+        `Quarter ${i + 1}`
+      );
+
+      const outer = number(
+        `Outer`,
+        Math.round(Math.random() * 10) / 10,
+        {
+          range: true,
+          min: 0,
+          max: 1,
+          step: 0.1
+        },
+        `Quarter ${i + 1}`
+      );
+
+      return {
+        inner: {
+          fill: i % 2 === 0 ? BrandColor.primary : BrandColor.secondary,
+          value: inner
+        },
+        outer: {
+          fill: i % 2 === 0 ? BrandColor.secondary : BrandColor.primary,
+          value: outer
+        }
+      };
+    });
+
+    return <Graph values={values as Tuple<ValueType, 6>} />;
+  });
