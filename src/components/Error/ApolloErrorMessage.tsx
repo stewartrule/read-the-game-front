7faedbd5dc9328aeaa572011568dc0ba2a269744
Walter Steur <stewartrule@gmail.com>
2019-10-26 14:32:13 +0200

    Add Apollo Error component

---
+++ src/components/Error/ApolloErrorMessage.tsx
@@ -0,0 +1,38 @@
+import { ApolloError } from "apollo-client";
+import * as React from "react";
+
+import Card, { CardColumn, CardSection } from "../Card";
+
+type Props = {
+  file: string;
+  error: ApolloError;
+  message: string;
+};
+
+const ApolloErrorMessage: React.FC<Props> = ({ error, message, file }) => {
+  if (process.env.NODE_ENV === "development") {
+    return (
+      <div className="error">
+        <p className="error__body">{message}</p>
+        <div className="error__info">
+          <p>{file}</p>
+          <p>{error.networkError ? error.networkError.name : ""}</p>
+          <p>{error.message}</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <Card>
+      <CardSection padding={[2, 1]}>
+        <CardColumn>
+          <h5>🤭</h5>
+          <p>{message}</p>
+        </CardColumn>
+      </CardSection>
+    </Card>
+  );
+};
+
+export default ApolloErrorMessage;
