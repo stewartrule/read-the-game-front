6957468c66683d3a3a529d0ea6ccf2af4f3d68fc
Walter Steur <stewartrule@gmail.com>
2019-11-14 10:05:27 +0100

    Add onClick to Tabs

--- src/components/TabGroup/Tab.tsx
+++ src/components/TabGroup/Tab.tsx
@@ -5,14 +5,17 @@ import bem from "../../util/bem";
 type Props = {
   active?: boolean;
   primary?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
 
 const Tab: React.FC<Props> = ({
   active = false,
   primary = false,
-  children
+  children,
+  onClick
 }) => (
   <button
+    onClick={onClick}
     className={bem({ tab: { "--active": active, "--primary": primary } })}
   >
     <span>{children}</span>
