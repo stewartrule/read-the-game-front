d8d5e582c30b734d4497b1bf809092792cf0a7b2
Walter Steur <stewartrule@gmail.com>
2019-11-14 10:15:09 +0100

    Add Websocket link to Apollo client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,22 +1,36 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
+import { createTransformerLink, isSubscription } from "apollo-client-transform";
+import { split } from "apollo-link";
 import { createHttpLink } from "apollo-link-http";
-import { createTransformerLink } from "apollo-client-transform";
+import { WebSocketLink } from "apollo-link-ws";
 
 import typeDefs from "./schema.gql";
 import { transformers } from "./transformers";
 
-const transformerLink = createTransformerLink(transformers);
+const host = 'localhost'
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3000/graphql"
+  uri: `http://${host}:3000/graphql`
 });
+
+const wsLink = new WebSocketLink({
+  uri: `ws://${host}:3000/graphql`,
+  options: {
+    reconnect: true,
+    lazy: true
+  }
+});
+
+const transformerLink = createTransformerLink(transformers);
+
+const link = split(({ query }) => isSubscription(query), wsLink, httpLink);
 
 const client = new ApolloClient({
   cache: new InMemoryCache().restore({}),
   connectToDevTools: true,
   typeDefs,
-  link: transformerLink.concat(httpLink)
+  link: transformerLink.concat(link)
 });
 
 export default client;
