a03c6439881213b9bfd1083255bd376f3194b2ab
Walter Steur <stewartrule@gmail.com>
2019-11-14 10:31:34 +0100

    Add ValidKeyOf utility type

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,3 +1,11 @@
 export type Tuple<T, L extends number> = [T, ...T[]] & { length: L };
 
 export type Maybe<T> = T | undefined
+
+export type ValueOf<T> = T[keyof T];
+
+export type ValidKeyOf<T, ValidKeyType> = ValueOf<
+  {
+    [K in keyof T]: T[K] extends ValidKeyType ? K : never;
+  }
+>;
