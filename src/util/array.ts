a8f0bea60b60bc8a4f966c2ce228dced3942ab96
Walter Steur <stewartrule@gmail.com>
2019-11-15 18:57:24 +0100

    Add minOf and maxOf array utilities

--- src/util/array.ts
+++ src/util/array.ts
@@ -1,3 +1,7 @@
+import { ValidKeyOf } from "./types";
+
+type AnyObject = Record<string, any>;
+
 export const chunk = <T>(input: T[], chunkSize = 3): T[][] => {
   const chunks: T[][] = [];
 
@@ -7,3 +11,19 @@ export const chunk = <T>(input: T[], chunkSize = 3): T[][] => {
 
   return chunks;
 };
+
+export const maxOf = <T extends AnyObject>(
+  items: T[],
+  key: ValidKeyOf<T, number>
+): number =>
+  items.reduce((max: number, item: T) => {
+    return item[key] > max ? item[key] : max;
+  }, -Infinity);
+
+export const minOf = <T extends AnyObject>(
+  items: T[],
+  key: ValidKeyOf<T, number>
+): number =>
+  items.reduce((max: number, item: T) => {
+    return item[key] < max ? item[key] : max;
+  }, Infinity);
